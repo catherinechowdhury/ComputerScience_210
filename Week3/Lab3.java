4417d06943c76652efc4013bac2bbf7d128034c4
Catherine Chowdhury <luna2soriano@gmail.com>
2026-02-12 13:30:17 -0500

    Modified Modulus

---
+++ Week3/Lab3.java
@@ -0,0 +1,34 @@
+package Week3;
+
+import java.util.Scanner;
+
+public class Lab3{
+
+    public static void main(String[] args){
+
+        // Declare a variable
+
+        // Initialize a variable
+
+        // Let's put it all together
+
+        // Print
+
+        // Valid Identifiers
+
+        // Camel Case
+
+        // What is a literal?
+        // String literals
+
+        // Integer literals
+
+        // Special characters
+
+
+
+    }
+
+
+
+}
---
+++ Week3/LabPractice.java
@@ -0,0 +1,37 @@
+package Week3;
+
+public class LabPractice {
+
+    public static void main(String[] args) {
+        // Declare a variable
+        int myVariable;
+
+        // Initialize a variable
+        myVariable = 10;
+
+        // Let's put it all together
+        int anotherVariable = 20;
+
+        // Print
+        System.out.println("The value of myVariable is: " + myVariable);
+        System.out.println("The value of anotherVariable is: " + anotherVariable);
+
+        // Valid Identifiers
+        int validIdentifier1;
+        int validIdentifier2;
+
+        // Camel Case
+        int camelCaseExample;
+
+        // What is a literal?
+        // String literals
+        String stringLiteral = "This is a string literal.";
+
+        // Integer literals
+        int integerLiteral = 100;
+
+        // Special characters
+        String specialCharacters = "This string contains special characters: \n\t\"\\";
+    }
+    
+}
